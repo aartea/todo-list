80ae6fbe0d2c29bb0e62cdaff3bf85b5c5e66145
aaron <aarongitis@gmail.com>
2016-04-14 15:07:18 -0700

    Add layout for sublist and sublist activity

--- app/src/main/java/co/aartea/to_dolist/sub_list.java
+++ app/src/main/java/co/aartea/to_dolist/sub_list.java
@@ -4,28 +4,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class sub_list extends AppCompatActivity {
+    //Instantiate
 
     ArrayList<String> elementList;
     ArrayAdapter<String> adapter;
 
     EditText et;
+    ListView lv;
 
-    String saveItem;
-    String getString;
+    ActionBar actionBar;
+
+    String saveItem, getString, saveFromMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,21 +34,33 @@ public class sub_list extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
+        //Initialize
         elementList = new ArrayList<>();
+        lv = (ListView) findViewById(R.id.listView);
         et = (EditText) findViewById(R.id.et);
+
         adapter = new ArrayAdapter<>
                 (sub_list.this,android.R.layout.simple_list_item_1, elementList);
 
+        //Set the adapter!
+        lv.setAdapter(adapter);
+
         Intent i = getIntent();
-        saveItem = i.getStringExtra("main_list");
+        saveFromMain = i.getStringExtra("main_list");
+
+        //Action bar - change the name from the source member
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.activity_sub_list);
+
+        //Set another intent to pass to another class to handle persistency
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 getString = et.getText().toString();
-                elementList.add(saveItem);
+                elementList.add(getString);
                 adapter.notifyDataSetChanged();
                 et.getText().clear();
                 //if successful, add a Snackbar saying so
